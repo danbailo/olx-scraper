81ce05776c1498162b572bbab96d7ca2dd5a40fb
Daniel Bailo <danbailoufms@gmail.com>
2019-10-25 14:26:25 -0400

    removido mobile a ajustado compatbilidade pra mais links

--- src/core/Olx.py
+++ src/core/Olx.py
@@ -17,19 +17,18 @@ class Olx:
         if not re.match(r".*(olx\.com\.br)", base_url):
             print("\nPor favor, insira um link da OLX, onde este deve conter alguma pesquisa!")
             exit(-1)
-        if re.match(r"https:\/\/m.olx",base_url):
-            base_url = re.sub(r"\&o=\d+","",base_url)
-            self.__pattern_mobile = re.compile(r"(.*?p\&)(.*)")
-            mobile_match = self.__pattern_mobile.match(base_url)
-            self.mobile_url = mobile_match[1]+"o=@@@&"+mobile_match[2]
-            self.desktop_url = False
-        else:
-            self.__pattern_desktop = re.compile(r"(.*?p\&)(.*)")
-            desktop_match = self.__pattern_desktop.match(base_url)
-            if desktop_match:
-                self.desktop_url = desktop_match[1]+"o=@@@&"+desktop_match[2]
-            else: self.desktop_url = re.sub(r"\?o\=\d+","",base_url)+"?o="
-            self.mobile_url = False
+        if re.match(r"(https:\/\/m\.olx\.com\.br.*)",base_url):
+            print("\nVocê inseriu um link mobile, por favor, entre com um link de uma pesquisa realizada num desktop!")
+            exit(-1)
+        pattern_desktop = re.compile(r"(.*?p\&)(.*)")
+        desktop_match = pattern_desktop.match(base_url)
+        if desktop_match:
+            self.desktop_url = desktop_match[1]+"o=@@@&"+desktop_match[2]
+            self.matched = True
+        else: 
+            self.desktop_url = re.sub(r"\?o\=\d+","",base_url)+"?o="
+            self.matched = False
+            
         self.__ad_link = {}
         self.__user_phone = {}
         self.__pattern_all = re.compile(r".*")      
@@ -38,30 +37,23 @@ class Olx:
         if sheet[-5:] != ".xlsx":
             self.__sheet = sheet+".xlsx"
         else: self.__sheet = sheet
-        
 
     def handler_ads(self,i):
         try:
-            if not self.mobile_url:
-                url = self.desktop_url
+            url = self.desktop_url
+            if self.matched:
                 response = requests.get(re.sub(r"@@@",str(i),url))
-            else:
-                url = self.mobile_url
-                response = requests.get(re.sub(r"@@@",str(i),url))
+            else: 
+                response = requests.get(url+str(i))
             if response.status_code != 200: return False
             response.close()
         except Exception:
             return False
         soup = BeautifulSoup(response.text,"html.parser")                
-        if not self.mobile_url:
-            ad_list = soup.find(name="div",attrs={"class":"section_OLXad-list"})
-            for ad in ad_list.findAll("a"):
-                self.__ad_link[ad["id"]] = ad["href"]
-        else:
-            ad_list = soup.find(name="ul",attrs={"id":"main-ad-list"})
-            for ad in ad_list.findAll("a"):
-                id_ad = ad["href"].split("-")[-1]
-                self.__ad_link[id_ad] = ad["href"]            
+        ad_list = soup.find(name="ul",attrs={"id":"main-ad-list"})
+        for ad in ad_list.findAll("a"):
+            id_ad = ad["href"].split("-")[-1]
+            self.__ad_link[id_ad] = ad["href"]            
 
     def get_ads(self):
         pool = ThreadPool(10)
@@ -72,7 +64,7 @@ class Olx:
             response = requests.get(ad)
             if response.status_code != 200: return False
             response.close()
-        except Exception:   return False            
+        except Exception: return False            
         soup = BeautifulSoup(response.text, "html.parser")
         script_data = soup.find("script",attrs={"data-json":self.__pattern_all})
         data = json.loads(script_data.get("data-json"))
@@ -87,10 +79,8 @@ class Olx:
     def write_sheet(self):
         workbook = xlsxwriter.Workbook(os.path.join("..","output",self.__sheet))
         worksheet = workbook.add_worksheet()
-
         phones_values = list(self.__user_phone.values())
         phones = [phone for phone in phones_values if phone != '']
-
         for row_number, phone_number in enumerate(phones):
             match_phone = self.__patern_cel.match(phone_number)
             if match_phone:
